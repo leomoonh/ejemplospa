8f20961233170192aa0952557d72920529e9ad11
Leo Leon <munguial@unam.mx>
2020-10-20 23:02:25 -0500

    Ejercicios SPA

---
+++ src/components/artist.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+  li
+    h2: a(:href="artist.url" target="_blank") {{ artist.name }}
+    img(:src="artist.image[2]['#text']")
+</template>
+
+<script>
+export default {
+  name: 'artist',
+  props: ['artist']
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: block;
+  margin: 50 0px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
+
